2158c00e5bcbd49ccb9608b0fdd89fc7cb4d8e44
Roman Frołow <rofrol@gmail.com>
2016-01-08 09:39:14 +0100

    formatting

--- proxy.sh
+++ proxy.sh
@@ -14,12 +14,12 @@ git config -f ${gitconfig_proxy_r} https.proxy ${proxy}
 git config -f ${gitconfig_proxy_r} http."${exception}".proxy ''
 git config -f ${gitconfig_proxy_r} https."${exception}".proxy ''
 git config --global include.path "${gitconfig_proxy}"
-
 # check with: git config --global -l
 
 #################################################################################
 # npm
 #################################################################################
+
 npm config set http-proxy ${proxy}
 npm config set https-proxy ${proxy}
 # check with: npm config ls
@@ -27,13 +27,14 @@ npm config set https-proxy ${proxy}
 #################################################################################
 # atom.io
 #################################################################################
+
 apm config set http-proxy ${proxy}
 apm config set https-proxy ${proxy}
 # check with: apm config ls
- 
 
 #################################################################################
 # gem
 #################################################################################
+
 echo "http_proxy: ${proxy}" > ~/.gemrc
 # check with: gem environment
