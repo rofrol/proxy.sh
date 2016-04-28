48913538be04cbf956197e45d372f8ff1b942c63
Roman Frołow <(none)>
2016-04-28 16:11:47 +0200

    remove else, readme and env

--- README.md
+++ README.md
@@ -8,16 +8,25 @@ Shell script to set proxy for various programs.
 sh proxy.sh <proxy> <exception>
 ```
 
+Or you can set env vars HTTP_PROXY and HTTP_PROXY_EXCEPTION and run
+
+```bash
+sh proxy.sh
+```
+
+There is also file `.env.example` and you can copy it to `.env` and set your config. Then you can use https://github.com/kennethreitz/autoenv to automatically source env variables from `.env` file whenever you cd to directory.
+
+
 ## Copmarision to setting env variables HTTP_PROXY and HTTPS_PROXY
 
-### Benefits
+### Pros
 
 - Single script run and everything works
 - You don't need to restart all the programs to reread env variables, when you unhibernate your machine and there is no proxy in the network
 - If you need to clone some repo which is accessible not through proxy, you don't need to unreset proxy like this `git clone --config "http.proxy="` and then reset proxy inside repo
 - You can have exceptions for git and bower
 
-### Drawbacks
+### Cons
 
 - You may need to set proxy for new programs
 
--- proxy.sh
+++ proxy.sh
@@ -1,41 +1,57 @@
 #!/bin/bash
 
-proxy="${1}"
-exception="${2}"
-gitconfig_proxy="~/.gitconfig_proxy"
-gitconfig_proxy_r="${gitconfig_proxy/#\~/$HOME}"
+# Provide settings as parameters
+[ ! -z "$1" ] && HTTP_PROXY=${1}
+[ ! -z "$2" ] && HTTP_PROXY_EXCEPTION=${2}
+
+if [ -z "$HTTP_PROXY" ]; then
+echo "set HTTP_PROXY or run: sh proxy.sh <proxy>"
+exit 1
+fi
+
+echo HTTP_PROXY=${HTTP_PROXY}
+echo HTTP_PROXY_EXCEPTION=${HTTP_PROXY_EXCEPTION}
+
+GITCONFIG_PROXY="~/.gitconfig_proxy"
+
+# git config -f needs $HOME and not ~
+GITCONFIG_PROXY_EXPANDED="${GITCONFIG_PROXY/#\~/$HOME}"
 
 #################################################################################
 # git
 #################################################################################
 
-git config -f ${gitconfig_proxy_r} http.proxy ${proxy}
-git config -f ${gitconfig_proxy_r} https.proxy ${proxy}
-git config -f ${gitconfig_proxy_r} http."${exception}".proxy ''
-git config -f ${gitconfig_proxy_r} https."${exception}".proxy ''
-git config -f ${gitconfig_proxy_r} url."https://".insteadOf git://
-git config --global include.path "${gitconfig_proxy}"
+git config -f ${GITCONFIG_PROXY_EXPANDED} http.proxy ${HTTP_PROXY}
+git config -f ${GITCONFIG_PROXY_EXPANDED} https.proxy "${HTTP_PROXY}"
+
+if [ ! -z "$HTTP_PROXY_EXCEPTION" ]; then
+git config -f ${GITCONFIG_PROXY_EXPANDED} http."${HTTP_PROXY_EXCEPTION}".proxy ''
+git config -f ${GITCONFIG_PROXY_EXPANDED} https."${HTTP_PROXY_EXCEPTION}".proxy ''
+fi
+
+git config -f ${GITCONFIG_PROXY_EXPANDED} url."https://".insteadOf git://
+git config --global include.path "${GITCONFIG_PROXY}"
 # check with: git config --global -l
 
 #################################################################################
 # npm
 #################################################################################
 
-npm config set http-proxy ${proxy}
-npm config set https-proxy ${proxy}
+npm config set http-proxy ${HTTP_PROXY}
+npm config set https-proxy ${HTTP_PROXY}
 # check with: npm config ls
 
 #################################################################################
 # atom.io
 #################################################################################
 
-apm config set http-proxy ${proxy}
-apm config set https-proxy ${proxy}
+apm config set http-proxy ${HTTP_PROXY}
+apm config set https-proxy ${HTTP_PROXY}
 # check with: apm config ls
 
 #################################################################################
 # gem
 #################################################################################
 
-echo "http_proxy: ${proxy}" > ~/.gemrc
+echo "http_proxy: ${HTTP_PROXY}" > ~/.gemrc
 # check with: gem environment
